b55906a9a0331704e6cd57422cba7533f92ac4fa
Brian Albertalli <brian.albertalli@gmail.com>
2018-06-17 10:51:47 -0400

    Cleaned up freqpoles.py

--- freqpoles.py
+++ freqpoles.py
@@ -6,7 +6,7 @@
 #   (See the equations in my 128th AES Convention paper.)
 #
 #   [P]=freqpoles(FR,Fs,Q) creates poles from the given frequency vector
-#   FR with sampling frequency Fs but it uses the quality factors 
+#   FR with sampling frequency Fs but it uses the quality factors
 #   given in Q vector to set the radius of the poles. (-3dB bandwidth
 #   is DeltaF=FR/Q.)
 #
@@ -14,23 +14,22 @@
 
 import numpy as np
 
-def freqpoles(fr, Fs = 44100, Q = None):
 
-	# discrete pole frequencies
-	wp = 2 * np.pi * fr / Fs 
+def freqpoles(fr, fs=44100, q=None):
+    """Discrete pole frequencies."""
+    wp = 2 * np.pi * fr / fs
 
-	dwp = []
-	if Q is None:
-		pnum = wp.size
-		dwp = np.zeros(pnum)
-		for k in range(1, pnum-1):
-			dwp[k] = (wp[k+1] - wp[k-1])/2
-		dwp[0] = wp[1] - wp[0]
-		dwp[pnum-1] = wp[pnum-1] - wp[pnum-2]
-	else:
-		dwp = wp / Q
-		
-	p = np.exp(-dwp/2) * np.exp(1j * wp)
-	
-	return np.hstack([p, p.conj()])
-	
+    dwp = []
+    if q is None:
+        pnum = wp.size
+        dwp = np.zeros(pnum)
+        for k in range(1, pnum - 1):
+            dwp[k] = (wp[k + 1] - wp[k - 1]) / 2
+        dwp[0] = wp[1] - wp[0]
+        dwp[pnum - 1] = wp[pnum - 1] - wp[pnum - 2]
+    else:
+        dwp = wp / q
+
+    p = np.exp(-dwp / 2) * np.exp(1j * wp)
+
+    return np.hstack([p, p.conj()])
